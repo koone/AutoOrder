59491514dccfee70bf1022ee2969190cc582a91c
Seaven <631766344@qq.com>
2015-04-03 18:21:53 +0800

    预约单

---
+++ autoorder/src/main/java/com/autoorder/bo/Order.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.autoorder.bo;
+
+/**
+ * 
+ * 预约单
+ * @author Seaven
+ *
+ */
+public class Order {
+
+	private Long id;
+	
+	
+	private String userName;
+	
+	private String userIDCard;
+	
+	private String usertelphone;
+	
+}
